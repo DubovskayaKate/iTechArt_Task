e5d90834717502ae22dfc4f92cb8cccf1a3a624a
Lapochka15 <dubovskayakatya56@gmail.com>
2019-08-20 14:06:50 +0300

    Created connection to DB using appsettings.json, User class

---
+++ Task3/MoneyManager/MoneyManager/ApplicationContext.cs
@@ -0,0 +1,16 @@
+﻿using  Microsoft.EntityFrameworkCore;
+using MoneyManagerClassLibrary;
+
+namespace MoneyManager
+{
+    public class ApplicationContext : DbContext
+    {
+
+        public ApplicationContext(DbContextOptions<ApplicationContext> options) : base(options)
+        {
+            //Database.EnsureCreated();
+        }
+
+        public DbSet<User> Users { get; set; }
+    }
+}
---
+++ Task3/MoneyManager/MoneyManager/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using Microsoft.Extensions.Configuration;
+using System.IO;
+using Microsoft.Extensions.Configuration.FileExtensions;
+using Microsoft.Extensions.Configuration.Json;
+using Microsoft.EntityFrameworkCore;
+
+namespace MoneyManager
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var builder = new ConfigurationBuilder();
+
+            builder.SetBasePath(Directory.GetCurrentDirectory());
+
+            builder.AddJsonFile("appsettings.json");
+
+            var config = builder.Build();
+
+            string connectionString = config.GetConnectionString("DefaultConnection");
+
+            var optionsBuilder = new DbContextOptionsBuilder<ApplicationContext>();
+            var options = optionsBuilder
+                .UseSqlServer(connectionString)
+                .Options;
+
+            using (ApplicationContext db = new ApplicationContext(options))
+            {
+                //Some actions
+            }
+
+
+            Console.ReadKey(true);
+        }
+    }
+}
