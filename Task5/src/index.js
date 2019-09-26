bc408fa2edd7ab9802640263478d820353aa61e5
Lapochka15 <dubovskayakatya56@gmail.com>
2019-09-26 12:53:29 +0300

    Created webpack.config

---
+++ Task5/src/index.js
@@ -0,0 +1,36 @@
+import './scss/footer/layout/col2/contact.scss';
+
+import './scss/content.scss';
+import './scss/startSettings.scss';   
+
+import './scss/footer/footer.scss';
+import './scss/footer/layout/col.scss';
+import './scss/footer/layout/info.scss';     
+import './scss/footer/layout/col2/contact.scss';
+import './scss/footer/layout/col2/icon.scss';
+
+import './scss/header/content.scss';
+import './scss/header/menu/item.scss';        
+import './scss/header/label/logo.scss';
+import './scss/header/label.scss';
+import './scss/header/label/headline.scss';
+import './scss/header/label/button.scss';
+
+import './scss/templates/bestseller-label.scss';
+import './scss/templates/sale-label.scss';
+import './scss/templates/text.scss';
+import './scss/templates/simple-button.scss';
+import './scss/templates/strong-text.scss';
+import './scss/templates/selected-item.scss';
+
+import './scss/main/grid.scss';
+import './scss/main/button.scss';
+import './scss/main/catalog-text.scss';        
+import './scss/main/main.scss';
+import './scss/main/sort-text.scss';
+import './scss/main/show-more-img.scss';
+import './scss/main/tour/image.scss';
+import './scss/main/tour/name.scss';
+import './scss/main/tour/info.scss';
+import './scss/main/tour/old-price.scss';
+import './scss/main/tour/price.scss';
