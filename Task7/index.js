562c6c2cf61594350c61ed2e0fe4f07a5b72a22c
Lapochka15 <dubovskayakatya56@gmail.com>
2019-10-04 12:50:46 +0300

    Implemented some jQuery methods

--- Task7/index.js
+++ Task7/index.js
@@ -14,47 +14,75 @@ const jQuery = {
         }
     },
 
-    append: function(selector, node){
-        let elements = document.querySelectorAll(selector);
-        console.log(elements);
-        for(let element of elements ) {
-            console.log(element.nodeName)
-            element.append(node);
-        }
+    append: function(selector, strHtml){
+        
     },
 
-    remove: function(selector, node){
-        //node.remove;
+    remove: function(node){
+        let element = document.querySelector(node);
+        element.remove();
     },
 
     text: function(node){
-        //node.childNodes
-        //return text
+        let element = document.querySelector(node);
+        return element.innerText;
     },
 
+    //Get the value of an attribute for the first element in the set of matched elements
+    //value != null set one or more attributes for every matched element.
     attr: function(selector, attributeName, value){
-        //Get the value of an attribute for the first element in the set of matched elements
-        //value != null set one or more attributes for every matched element.
+        let element = document.querySelector(selector);
+        if (value){
+            element.setAtttibute(attributeName, value);
+        }
+        else{
+            return element.getAttribute(attributeName);            
+        }
     },
 
-    children: function (selector){
-        //Get the children of each element in the set of matched elements, optionally filtered by a selector.
+    //Get the children of each element in the set of matched elements, optionally filtered by a selector.
+    children: function (node, selector){
+        let element = document.querySelector(node);
+        let result = [];
+        if (!selector)
+            return element.childNodes;
+
+        for (let child of element.childNodes){            
+            if (child.nodeType == 1 && child.matches(selector)){
+                result.push(child);
+            }
+        }
+        return result;
     },
 
-    empty: function(){
-        //Remove all child nodes of the set of matched elements from the DOM.
+    //Remove all child nodes of the set of matched elements from the DOM.
+    empty: function(node){
+        let element = document.querySelector(node);
+        
+        while (element.firstChild){
+            let child = element.firstChild;
+            child.remove();    
+        }        
     },
 
-    css: function(selector, property){
-        //Get value of property
+    css: function(node, property){
+        let element = document.querySelector(node);
+        element.setAttribute("style", property);
     },
 
-    click: function(selector, callback){
-        //Bind an event handler to the "click" JavaScript event,
+    click: function(node, callback){
+        let element = document.querySelector(node);
+        element.addEventListener('click', callback);
     }
 }
 
 jQuery.addClass('body', 'katya');
 jQuery.removeClass('body', 'katya');
-jQuery.append('body', '<h2> Hello, its me </h2>');
-console.log(document.body.classList);
+jQuery.remove('li');
+//jQuery.empty('ul');
+//jQuery.css('body', 'background-color: green');
+console.log(jQuery.children(".demo-container", 'ul'));
+console.log(jQuery.attr(".demo-box", 'something'));
+console.log(jQuery.click('body', function() {
+    alert("addEventListener"); 
+  }))
